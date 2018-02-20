2868054b9002c3c18b5dbb47c14a7b3bd6cbdaaa
Junian <git@junian.net>
2018-02-20 15:35:59 +0700

    fix fibonacci generator

--- fibonacci-series-generator/fibonacci-series-generator.cpp
+++ fibonacci-series-generator/fibonacci-series-generator.cpp
@@ -1,33 +1,33 @@
 //Pencetak Deret Fibonacci
 
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
 
 char toUpper(char);
 
-main(){
+int main(){
   char r;
   do{
     int n, x = 0, y = 1, z = 1;
     system("cls");
-    printf("Pencetak Deret Fibonacci\n");
-    printf("========================\n\n");
+    printf("Fibonacci Series Generator\n");
+    printf("==========================\n\n");
     printf("Input N : ");
     scanf("%d", &n);
-    printf("\nDeret Fibonacci Kurang dari %d:\n\n", n);
+    printf("\nFibonacci series less than %d:\n\n", n);
     while(z < n){
       printf("%5d", z);
       z = x + y;
       x = y;
       y = z;
     }
-    printf("\n\nIngin Mengulang [Y/N]? ");
+    printf("\n\nTry again [Y/N]? ");
     do{
-      r = getch();
+      r = getchar();
       r = toUpper(r);
     }while(r != 'Y' && r != 'N');
   }while(r == 'Y');
+  return 0;
 }
 
 char toUpper(char x){
